4aab4edec8073f1bd15149717847269ed420373c
zvn90 <115043149+zvn90@users.noreply.github.com>
2023-01-28 00:26:12 +0300

    Задание № 3.1

---
+++ data-types.js
@@ -0,0 +1,19 @@
+const name = 'Вадим'
+const age = 33
+const heIsStudent = true
+const surname = null
+let time
+const card = {
+ name: 'Вадим',
+ age: 33}
+const id = Symbol('id')
+const bigNumber = 10n
+
+console.log(name, typeof name)
+console.log(age, typeof age)
+console.log(heIsStudent, typeof heIsStudent)
+console.log(surname, typeof surname)
+console.log(time, typeof time)
+console.log(card, typeof card)
+console.log(id, typeof id)
+console.log(bigNumber, typeof bigNumber)

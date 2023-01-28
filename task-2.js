9a8457e1a74bf76286f288ab122a5a04f1383195
zvn90 <115043149+zvn90@users.noreply.github.com>
2023-01-28 23:02:22 +0300

    Задание №

--- task-2.js
+++ task-2.js
@@ -38,3 +38,24 @@ console.log(Number(''), Boolean(''), String(''))
 console.log(Number(0), Boolean(0), String(0))
 console.log(Number(-10), Boolean(-10), String(-10))
 console.log(Number('-105'), Boolean('-105'), String('-105'))
+
+// 1
+console.log(Number(' 1 2 3 4 5'));//NaN
+// 2
+console.log(Number('1234 5'));//NaN
+// 3
+console.log(Number('12345'));//12345
+// 4
+console.log(String(false));//false
+// 5
+console.log(Boolean(0000000));//false
+// 6
+console.log(Boolean(0.0000001));//true
+// 7
+console.log(String(undefined));//undefined
+// 8
+console.log(Number('100f'));//Nan
+// 9
+console.log(Number('100'));//100
+// 10
+console.log(Number('000001'));//1

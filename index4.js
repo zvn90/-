ee74d483a4f6db0076ac5eb9262ba364aa31a4fb
zvn90 <115043149+zvn90@users.noreply.github.com>
2023-01-28 22:51:07 +0300

    Задание № 4.1

---
+++ index4.js
@@ -0,0 +1,19 @@
+let name = 'Вадим'
+let age = 33
+let heIsStudent = true
+let surname = null
+let time
+let card = {
+ name: 'Вадим',
+ age: 33}
+let id = Symbol('id')
+let bigNumber = 10n
+
+console.log(Number(name), Boolean(name), String(name))
+console.log(Number(age), Boolean(age), String(age))
+console.log(Number(heIsStudent), Boolean(heIsStudent), String(heIsStudent))
+console.log(Number(surname), Boolean(surname), String(surname))
+console.log(Number(time), Boolean(time), String(time))
+console.log(Number(card), Boolean(card), String(card))
+console.log(Boolean(id), String(id))
+console.log(Number(bigNumber), Boolean(bigNumber), String(bigNumber))
